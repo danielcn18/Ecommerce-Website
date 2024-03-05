1ba565a00ee181670bccf417b8930221ff6ade03
Daniel Chiquete <dchiquete2019@gmail.com>
2024-03-05 14:05:11 -0700

    Added general pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Home from './pages/Home';
 
 export default function App() {
 
   return (
     <>
-      
+      <Home />
+      {/* <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' /> 
+        <Route path='/contact' /> 
+        <Route path='/calculate' /> 
+      </Routes> */}
     </>
   )
 }
