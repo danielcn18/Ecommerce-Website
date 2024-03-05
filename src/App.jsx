4509c1a06fa19f937e9313db925c6d5f70814c96
Daniel Chiquete <dchiquete2019@gmail.com>
2024-03-05 14:28:20 -0700

    end of class changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Home from './pages/Home';
+import Header from './components/Header';
 
 export default function App() {
 
   return (
     <>
+      <Header />
       <Home />
       {/* <Routes>
         <Route path='/' element={<Home />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+// import '../styles/Header'; // Error here with path
+// import '../global';
+
+export default function Header() {
+
+    
+    return (
+        <div className='container'>
+
+        </div>
+    )
+}
