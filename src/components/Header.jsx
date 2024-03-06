e2f258304c221f32c304541e8dbfb65a514bf4b7
Daniel Chiquete <dchiquete2019@gmail.com>
2024-03-06 14:24:48 -0700

    work from in classroom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
-// import '../styles/Header'; // Error here with path
+import '../styles/Header.css'; // Error here with path
 // import '../global';
 
 export default function Header() {
-
     
     return (
         <div className='container'>
-
+            <img src=''></img>
+            <h1>Heading</h1>
         </div>
     )
 }
