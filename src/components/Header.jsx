092d8bb9673465e54a230deeb7ba69cf8d1bc4f1
Daniel Chiquete <dchiquete2019@gmail.com>
2024-03-07 14:25:14 -0700

    new changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,36 @@
 import '../styles/Header.css'; // Error here with path
+import logo from '../../public/logo_example.png'
 // import '../global';
 
 export default function Header() {
     
     return (
-        <div className='container'>
-            <img src=''></img>
-            <h1>Heading</h1>
-        </div>
-    )
-}
+        <ul className='header'>
+            <li>
+                <img src={logo} />
+            </li>
+            <li>
+                <p>Heading</p>
+            </li>
+            <li>
+                <p>Shop</p>
+            </li>
+            <li>
+                <p>Calculate Costs</p>
+            </li>
+            <li>
+                <p>Contact Us</p>
+            </li>
+            <li>
+                <p>Log In</p> {/* Log In / Username */}
+                <img alt='login_icon' />
+            </li>
+            <li>
+                <img alt='search_icon' /> {/* search bar */}
+            </li>
+            <li>
+                <img alt='shop_icon' /> {/* shopping cart */}
+            </li>
+        </ul>
+    )    
+}   
