6f23db7502ec94d9174160270397325990653a6e
Daniel Chiquete <dchiquete2019@gmail.com>
2024-03-11 14:26:28 -0700

    updated monday after class

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,17 @@
 import '../styles/Home.css';
+import '../global.css';
+import stockimage1 from '../../public/stockimage1.jpg'
 
 export default function Home() {
 
     return(
         <div className='main-container'>
             <div className='full-length-crop right'>
-                <div>
-                    <p>Build A Life Long Relationship With A Friend</p>
+                <img className='image-crop' /* src={stockimage1} */ alt='photo' />
+                <div className='right-child'>
+                    <p>Build A Life Long<br />Relationship With A Friend</p>
                     <button>Discover</button>
                 </div>
-                <img className='' src='' alt='photo' />
-            </div>
-            <div className='full-length-crop right'>
-                <div>
-                    <p>Build A Life Long Relationship With A Friend</p>
-                    <button>Discover</button>
-                </div>
-                <img className='' src='' alt='photo' />
             </div>
         </div>
     )
