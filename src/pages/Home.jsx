758a3a3d7c8633953d0d707e80a547ebd1b84c43
Daniel Chiquete <dchiquete2019@gmail.com>
2024-03-12 14:11:11 -0700

    updated changes from end of class, Monday

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,27 @@ export default function Home() {
     return(
         <div className='main-container'>
             <div className='full-length-crop right'>
-                <img className='image-crop' /* src={stockimage1} */ alt='photo' />
-                <div className='right-child'>
+                <img className='image-crop full' /* src={stockimage1} */ alt='photo' />
+                <div className='full-length-inner right-child'>
                     <p>Build A Life Long<br />Relationship With A Friend</p>
                     <button>Discover</button>
                 </div>
             </div>
+            <div className='half-length-crop'>
+                <img className='image-crop half' /* src={stockimage1} */ alt='photo' />
+                <div className='half-length-inner'>
+                    <p>Build A Life Long<br />Relationship With A Friend</p>
+                    <button>Discover</button>
+                </div>
+            </div>
+            <div className='half-length-crop'>
+                <img className='image-crop half' /* src={stockimage1} */ alt='photo' />
+                <div className='half-length-inner'>
+                    <p>Build A Life Long<br />Relationship With A Friend</p>
+                    <button>Discover</button>
+                </div>
+            </div>
+            
         </div>
     )
 }
