a2b8921d009ca942dc51bdec29c488fedec1d156
Daniel Chiquete <dchiquete2019@gmail.com>
2024-04-17 14:15:31 -0700

    forgot to changes jsx file class name

--- src/components/HalfView.jsx
+++ src/components/HalfView.jsx
@@ -3,7 +3,7 @@
 export default function HalfView({text, bText}) {
 
     return (
-        <div className="half-view">
+        <div className="card">
             <p>{text}</p>
             <button>{bText}</button>
         </div>
